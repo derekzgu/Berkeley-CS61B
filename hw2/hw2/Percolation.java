85f6c3a64613a23daf2c84dc937bf563399f6bd5
wendingp <wendingp@gmail.com>
2017-02-18 13:21:21 +0800

    Finish Hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,103 @@
-package hw2;                       
+package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-}                       
+    private class Site {
+        boolean isOpen;
+        int index;
+
+        public Site() {
+            isOpen = false;
+            index = 0;
+        }
+    }
+
+    private int virtualTop;
+    private int virtualBottom;
+
+    private WeightedQuickUnionUF weightedQuickUnionUF;
+    private WeightedQuickUnionUF weightedQuickUnionUFWithoutBOTTOM;
+
+    private Site[][] grid;
+    private int side;
+    private int numberOfOpenSites;
+
+    private final int dx[] = {-1, 0, 0, 1};
+    private final int dy[] = {0, -1, 1, 0};
+
+    /* Change item of the 2D array to an index. */
+    private int xyToIndex(int row, int col) {
+        return row * side + col;
+    }
+
+    /* Creates N by N grid, with all sites initially blocked. */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("number of grid in each side of square is not positive");
+        }
+        weightedQuickUnionUF = new WeightedQuickUnionUF(N * N + 2);
+        weightedQuickUnionUFWithoutBOTTOM = new WeightedQuickUnionUF(N * N + 1);
+        grid = new Site[N][N];
+        side = N;
+        virtualTop = N * N;
+        virtualBottom = N * N + 1;
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                grid[i][j] = new Site();
+                grid[i][j].index = xyToIndex(i, j);
+            }
+        }
+        numberOfOpenSites = 0;
+    }
+
+    public void open(int row, int col) {
+        if (row < 0 || col < 0 || row >= side || col >= side) {
+            throw new IllegalArgumentException("row and column is not in the range of [0, side - 1]");
+        }
+        if (!isOpen(row, col)) {
+            ++numberOfOpenSites;
+            grid[row][col].isOpen = true;
+        }
+        if (row == 0) {
+            weightedQuickUnionUF.union(virtualTop, grid[row][col].index);
+            weightedQuickUnionUFWithoutBOTTOM.union(virtualTop, grid[row][col].index);
+        } else if (row == side - 1) {
+            weightedQuickUnionUF.union(virtualBottom, grid[row][col].index);
+        }
+        for (int i = 0; i < 4; ++i) {
+            int neighborX = dx[i] + row;
+            int neighborY = dy[i] + col;
+            if (0 <= neighborX && neighborX < side && 0 <= neighborY && neighborY < side
+                    && grid[neighborX][neighborY].isOpen) {
+                /* This neighbor site is valid and open. */
+                weightedQuickUnionUF.union(grid[neighborX][neighborY].index, grid[row][col].index);
+                weightedQuickUnionUFWithoutBOTTOM.union(grid[neighborX][neighborY].index, grid[row][col].index);
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || col < 0 || row >= side || col >= side) {
+            throw new IllegalArgumentException("row and column is not in the range of [0, side - 1]");
+        }
+        return grid[row][col].isOpen;
+    }
+
+
+    public int numberOfOpenSites() {
+        return numberOfOpenSites;
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row < 0 || col < 0 || row >= side || col >= side) {
+            throw new IllegalArgumentException("row and column is not in the range of [0, side - 1]");
+        }
+        return weightedQuickUnionUFWithoutBOTTOM.connected(virtualTop, grid[row][col].index);
+    }
+
+    public boolean percolates() {
+        return weightedQuickUnionUF.connected(virtualTop, virtualBottom);
+    }
+}
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,58 @@
-package hw2;                       
+package hw2;
+
+import edu.princeton.cs.introcs.StdStats;
+
+import static edu.princeton.cs.introcs.StdRandom.uniform;
+import static java.lang.Math.sqrt;
 
 public class PercolationStats {
+    /* Perform T independent experiments on an N-by-N grid. */
+    private int N, T;
+    private double mean, stddev;
+    private double confidenceLow, confidenceHigh;
 
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("arguments are not positive integers");
+        }
+        double[] sample = new double[T];
+        /* Do T experiments. */
+        for (int i = 0; i < T; ++i) {
+            Percolation percolationExperiment = new Percolation(N);
+            do {
+                percolationExperiment.open(uniform(N), uniform(N));
+            } while (!percolationExperiment.percolates());
+            sample[i] = percolationExperiment.numberOfOpenSites() / (double) (N * N);
+        }
+        mean = StdStats.mean(sample);
+        stddev = StdStats.stddev(sample);
+        confidenceLow = mean - 1.96 * stddev / sqrt(T);
+        confidenceHigh = mean + 1.96 * stddev / sqrt(T);
+    }
+
+    public double mean() {
+        return mean;
+    }
+
+    public double stddev() {
+        return stddev;
+    }
+
+    public double confidenceLow() {
+        return confidenceLow;
+    }
+
+    public double confidenceHigh() {
+        return confidenceHigh;
+    }
+
+    /* Test */
+    public static void main(String[] args) {
+        PercolationStats percolationStats = new PercolationStats(20, 100000);
+        System.out.println("Mean: " + percolationStats.mean());
+        System.out.println("Standard Deviation: " + percolationStats.stddev());
+        System.out.println("Confidence Interval: [" + percolationStats.confidenceLow() + ", " +
+                percolationStats.confidenceHigh() + "]");
+
+    }
 }                       
